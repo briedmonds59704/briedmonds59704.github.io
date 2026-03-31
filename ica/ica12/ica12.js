7a5783c7811dc5836919eb7e1d00b72592cd5f7e
briedmonds59704 <briedmonds59704@gmail.com>
2026-03-31 10:11:48 -0600

    progress

--- ica/ica12/ica12.js
+++ ica/ica12/ica12.js
@@ -1,1 +1,17 @@
 let newBtn = document.querySelector('#js-new-quote');
+
+newBtn.addEventListener('click', getQuote);
+
+async const endPoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion';
+
+function getQuote() {
+   //alert("Success");
+    try {
+        const response = await fetch(endPoint);
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        const json = await response.json();
+        console.log(json);
+    }
+}
